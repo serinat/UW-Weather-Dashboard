49d6b97e691c05b085d945589e97ba066a77b2ec
Serina Tsang <serinat@uw.edu>
2019-10-23 02:10:40 -0700

    fixed layout, icon working

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+var now = moment().format('L');
+$(".date").append(now);
+
 //pull information from the form and build the query URL
 //function buildQueryURL() {
 // queryURL is the url we'll use to query the API
@@ -20,10 +23,12 @@ $.ajax({
         // Log the resulting object
         console.log(response);
 
-        $(".city").html("<h1>" + response.name + "</h1>");
-        $(".temp").text("Temperature (F) " + response.main.temp);
-        $(".humidity").text("Humidity: " + response.main.humidity);
-        $(".wind").text("Wind Speed: " + response.wind.speed);
+        $(".city").html("<h2>" + response.name + "</h2>");
+        $(".temp").text("Temperature " + response.main.temp + "F");
+        $(".humidity").text("Humidity: " + response.main.humidity + "%");
+        $(".wind").text("Wind Speed: " + response.wind.speed + "MPH");
+        $(".icon").html("<img src='http://openweathermap.org/img/w/" + response.weather[0].icon + ".png'>");
+
     });
 
 var APIKey = "4ba053499928c9cc55c84c5428ed0660";
@@ -44,3 +49,28 @@ $.ajax({
 
         $(".uv").text("UV index: " + response.value);
     });
+
+var APIKey = "4ba053499928c9cc55c84c5428ed0660";
+var queryURL = "http://api.openweathermap.org/data/2.5/forecast?q=London,us&mode=xml&appid=" + APIKey;
+
+//build object to contain API call's query parameters
+//set API key
+//var queryParams = { "api-key": "4ba053499928c9cc55c84c5428ed0660" };
+$.ajax({
+    url: queryURL,
+    method: "GET"
+})
+    // We store all of the retrieved data inside of an object called "response"
+    .then(function (response) {
+
+        // Log the queryURL
+        console.log(queryURL);
+
+        // Log the resulting object
+        console.log(response);
+
+        $(".forecast").text("5-Day forecast: " + response.list);
+
+    });
+
+
