8a59c22feedb182163bb002ae51d6bbaf923da79
Serina Tsang <serinat@uw.edu>
2019-10-26 06:11:06 -0700

    ajax call working and added temp param

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
 //$(document).ready(function () {
-//$("#submitWeather").on("click", function (event) {
-    //event.preventDefault();
+$("#submitWeather").on("click", function (event) {
+    event.preventDefault();
     //get date now
     var now = moment().format('L');
     $(".date").append(now);
 
     //grab text user types into search input
-    //var city = $("#city").val();
+    var city = $("#city").val();
 
     //pull information from the form and build the query URL
     //function buildQueryURL() {
     // queryURL is the url we'll use to query the API
     var APIKey = "4ba053499928c9cc55c84c5428ed0660";
-    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=London&appid=" + APIKey;
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial" + "&appid=" + APIKey;
 
     //build object to contain API call's query parameters
     //set API key
@@ -33,40 +33,40 @@
         url: queryURL,
         method: "GET"
     })
-    
-    .then(function (response) {
-        console.log(queryURL);
-        console.log(response);
 
-        //var weather = $("<div></div>");
-        $(".city").text(response.name);
-        $(".temp").text("Temperature " + response.main.temp + "F");
-        $(".humidity").text("Humidity: " + response.main.humidity + "%");
-        $(".wind").text("Wind Speed: " + response.wind.speed + "MPH");
-        $(".icon").html("<img src='http://openweathermap.org/img/w/" + response.weather[0].icon + ".png'>");
+        .then(function (response) {
+            console.log(queryURL);
+            console.log(response);
 
-        //var weather = $("<div></div>");
-        //var temp = $(".temp").text("Temperature " + temp + "F");
-        //var humidity = $(".humidity").text("Humidity: " + humidity + "%");
-        //var wind = $(".wind").text("Wind Speed: " + wind + "MPH");
-        //var icon = $(".icon").html("<img src='http://openweathermap.org/img/w/" + icon + ".png'>");
+            //var weather = $("<div></div>");
+            $(".city").text(response.name);
+            $(".temp").text("Temperature: " + (response.main.temp) + "F");
+            $(".humidity").text("Humidity: " + response.main.humidity + "%");
+            $(".wind").text("Wind Speed: " + response.wind.speed + "MPH");
+            $(".icon").html("<img src='http://openweathermap.org/img/w/" + response.weather[0].icon + ".png'>");
 
-        //weather.append(temp, humidity, wind, icon);
+            //var weather = $("<div></div>");
+            //var temp = $(".temp").text("Temperature " + temp + "F");
+            //var humidity = $(".humidity").text("Humidity: " + humidity + "%");
+            //var wind = $(".wind").text("Wind Speed: " + wind + "MPH");
+            //var icon = $(".icon").html("<img src='http://openweathermap.org/img/w/" + icon + ".png'>");
 
-
-        //event.preventDefault();
-        //var queryURL = buildQueryURL();
+            //weather.append(temp, humidity, wind, icon);
 
 
+            //event.preventDefault();
+            //var queryURL = buildQueryURL();
 
-    });
+
+
+        });
 
 
     //AJAX call for uv index
 
     var APIKey = "4ba053499928c9cc55c84c5428ed0660";
     var queryURL = "https://api.openweathermap.org/data/2.5/uvi?lat=37.75&lon=-122.37&appid=" + APIKey;
-    
+
     $.ajax({
         url: queryURL,
         method: "GET"
@@ -105,3 +105,4 @@
             $(".forecast").text("5-Day forecast: " + response.list);
 
         });
+});
